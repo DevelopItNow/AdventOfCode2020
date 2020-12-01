01987412b78449d0b496ef5eb06e8bf523bb5e85
DevelopItNow <kvnjager@gmail.com>
2020-12-01 08:56:51 +0100

    Day1 Part1

--- AOC2020/Days/Day1.cs
+++ AOC2020/Days/Day1.cs
@@ -6,6 +6,20 @@ namespace AOC2020.Days
 	{
 		public Day1()
 		{
+			string[] programList = Utils.LoadFile("Day1-1.txt");
+			foreach (string i in programList)
+			{
+				foreach (string j in programList)
+				{
+					int num1 = Int32.Parse(i);
+					int num2 = Int32.Parse(j);
+					if ((num1 + num2) == 2020)
+					{
+						Console.WriteLine(num1 * num2);
+						return;
+					}
+				}
+			}
 		}
 	}
 }
